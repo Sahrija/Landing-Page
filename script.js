b810024add79bb135fca1a2688209cb765bfd777
Sahrija <114372392+Sahrija@users.noreply.github.com>
2022-11-28 14:10:30 +0700

    add files

---
+++ script.js
@@ -0,0 +1,20 @@
+let pill = document.querySelector(".pill");
+let pillNav = document.querySelector(".pill-nav");
+let navitem = document.querySelectorAll("nav-item")
+
+pill.onclick = function(){
+    pillNav.classList.toggle("aktif")
+}
+// ssssssssssssssssssssssssssssss
+
+
+let list = document.querySelectorAll('.nav-item');
+
+function aktiflink() {
+    list.forEach((item) =>
+        item.classList.remove('aktif'));
+    this.classList.add('aktif')
+}
+
+list.forEach((item) =>
+    item.addEventListener('click', aktiflink))
