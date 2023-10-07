05966e872293cdac437e5c0369465b3edf9d6a9b
Sahrija <114372392+Sahrija@users.noreply.github.com>
2023-10-07 13:09:39 +0700

    update js

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
 
-// ssssssssssssssssssssssssssssss
-
-
-let list = document.querySelectorAll('.nav-item');
-
-function aktiflink() {
-    list.forEach((item) =>
-        item.classList.remove('aktif'));
-    this.classList.add('aktif')
-}
-
-list.forEach((item) =>
-    item.addEventListener('click', aktiflink));
-
 // category toggle
 
 
 let categoryButton = document.querySelectorAll('.category-item');
-let categoryGroup = document.querySelectorAll('.category-item');
 
 function aktiflink() {
     categoryButton.forEach((item) =>
